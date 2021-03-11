e4316c413dccdeb65684e3beff690b0ab41071ac
kirtimistry2001 <kirtimistry2001@yahoo.com>
2021-03-11 16:54:46 -0500

    added target folder to ignore

--- src/test/java/ui/LoginTest.java
+++ src/test/java/ui/LoginTest.java
@@ -21,7 +21,6 @@ public class LoginTest {
 		} 
 		if (driver != null) {
 		driver.get("https://www.saucedemo.com/");
-		//*[@id="user-name"]
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
 		driver.findElement(By.id("password")).sendKeys("secret_sauce");
 //		driver.findElement(By.id("login-button")).click();
